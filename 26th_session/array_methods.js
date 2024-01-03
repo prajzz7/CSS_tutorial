4c980eefb409d8bde204171e074b1ad5083e4ec7
prajzz7 <pandeshwarprajwal@gmail.com>
2024-01-04 02:51:23 +0530

    26th session completed

---
+++ 26th_session/array_methods.js
@@ -0,0 +1,79 @@
+const array = [1,2,3,7]
+const array2 = [66,77,71,88,99,111,71,222,333,71,12]
+const items = ['wallet','cap','bag','key','belt']
+const items2 = ['wallet','cap']
+
+
+
+// const new_length = array.push(9)
+// console.log(new_length)
+
+// const elemnt = array.pop()
+// console.log(elemnt)
+
+// const element = array.shift(99)
+// console.log(element)
+// console.log(array)
+
+// const new_l = array.unshift(99)
+// console.log(new_l)
+// console.log(array)
+
+// const joinedAraay = array.concat(array2)
+// console.log(joinedAraay)
+
+// const new_arr = items.join(';')
+// console.log(new_arr)
+
+// array2.splice(1,0,55)
+// console.log(array2)
+
+// console.log(array2.slice(1,4))
+
+
+// array2.forEach(function(element,index){
+//     console.log('element:',element, 'index:',index)
+// })
+
+// for (element of array2){
+//     console.log(element)
+// }
+
+// for(var i=0; i<array2.length;i++){
+
+//     if(i===2){
+        
+//         break
+//     }
+//     console.log(i)
+// }
+
+// const boolean = array2.every(function(element){
+//     return element>60
+// })
+// console.log(boolean)
+
+// const boolean = array2.some(function(element){
+//     return element>400
+// })
+// console.log(boolean)
+
+// const num = array2.find(function(element){
+//     return element>70
+// })
+// console.log(num)
+
+// console.log(array2.includes(70))
+// console.log(array2.indexOf(71))
+// console.log(array2.lastIndexOf(71))
+
+// console.log(items.sort())
+// console.log(array2.sort(function(a,b){
+//     return b-a
+// }))
+
+// console.log(items.reverse())
+
+
+
+// const a = [1,2,3,5,6,7]
---
+++ 26th_session/non-primitive.js
@@ -0,0 +1,19 @@
+//Primitive eg
+
+// var a =1;
+// var b = a;
+// b=2;
+// console.log(a)
+
+//Non-primitive eg
+//Shallow copy
+
+// const array = [1,2,3]
+// const new_arr = array
+// new_arr[0] = 99
+// console.log(array)
+
+// const new_arr = JSON.parse(JSON.stringify(array)) //DEEP COPY
+// new_arr[0]=99
+// console.log(array)
+
