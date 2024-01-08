654fa63d5b69644056f72061f8de5c6a3524e764
prajzz7 <pandeshwarprajwal@gmail.com>
2024-01-08 21:05:37 +0530

    28 session completed

---
+++ 28th_session/loops.js
@@ -0,0 +1,38 @@
+// const obj = {
+//     contact: 123,
+//     name:{
+//         f_name: 'P',
+//         l_name:'L'
+//     },
+//     func: function(){
+//         return null
+//     }
+// }
+
+// let shallow = obj
+// shallow.contact = 456
+// console.log(obj)
+
+// for (i=0;i<5;i++){
+//     console.log(i)
+// }
+
+
+// for(let keys in obj){
+//     console.log(keys, obj[keys])
+// }
+
+// console.log(obj.contact)
+var i = 5
+// while(i<8){
+//     console.log(i)
+//     i++
+// }
+
+do{
+    console.log(i)
+    i++
+}while(i<8)
+
+
+
