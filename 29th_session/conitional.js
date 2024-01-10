702707bc16eb3dda5badfa6e72047e7901ba3f3f
prajzz7 <pandeshwarprajwal@gmail.com>
2024-01-10 21:14:42 +0530

    29th session completed

---
+++ 29th_session/conitional.js
@@ -0,0 +1,52 @@
+// const object = {
+//     property: 'value'
+// }
+
+
+// const data = object.name?.()
+// console.log(data)
+
+//Ternary operator
+// var message;
+// if(age>=18){
+//     message = 'You are eligible' 
+// }
+// else{
+//     message= 'Not eligible'
+// }
+// var age = 15
+// let message = (age>=18)?'You are eligible':'Not eligible'
+// console.log(message)
+
+// && ||
+
+// const username = prompt('Enter your username')
+// const password = prompt('Enter password')
+
+// const validUsername = 'admin'
+// const validPassword = 'pass123'
+
+// if(username === validUsername || password === validPassword){
+//     console.log('User is authenticated')
+// }
+// else{
+//     console.log('Not authenticated')
+// }
+
+
+let key =4
+
+switch(key){
+    case 1:
+        console.log('Key 1')
+        break;
+    case 2:
+        console.log('Key 2')
+        break;
+    case 3:
+        console.log('Key 3')
+        break;
+    default:
+        console.log('Invalid key');
+
+}
